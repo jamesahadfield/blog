835cdbaf739f8e47978ff9bc6839c3ffa78ca533
James <jameshadfield94@gmail.com>
2024-07-28 11:06:07 +0100

    fix build

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import fs from "fs";
 import matter from "gray-matter";
 import path from "path";
-import { getPostData } from "../../../lib/posts";
+import { getPostData, getSortedPostsData } from "../../../lib/posts";
 
 type Params = {
   slug: string;
@@ -17,7 +17,6 @@ type PostData = {
   contentHtml: string;
 };
 
-const postsDirectory = path.join(process.cwd(), "posts");
 
 export async function generateMetadata({ params }: Props) {
   const postData: PostData = await getPostData(params.slug);
@@ -27,35 +26,17 @@ export async function generateMetadata({ params }: Props) {
   };
 }
 
-// export async function generateStaticParams() {
-//   const files = fs.readdirSync(postsDirectory);
+export async function generateStaticParams() {
+  const allPostsData = await getSortedPostsData();
 
-//   const allPostsData = files.map((fileName) => {
-//     // Remove ".md" from file name to get id
-//     const id = fileName.replace(/\.md$/, "");
-
-//     // Read markdown file as string
-//     const fullPath = path.join(postsDirectory, fileName);
-//     const fileContents = fs.readFileSync(fullPath, "utf8");
-
-//     // Use gray-matter to parse the post metadata section
-//     const matterResult = matter(fileContents);
-
-//     // Combine the data with the id
-//     return {
-//       id,
-//       data: matterResult.data,
-//     };
-//   });
-
-//   return allPostsData.sort((a, b) => {
-//     if (a.data["date"] < b.data.date) {
-//       return 1;
-//     } else {
-//       return -1;
-//     }
-//   });
-// }
+  return allPostsData.map((post) => {
+    return {
+      params: {
+        slug: post.id,
+      },
+    };
+  });
+}
 
 export default async function Page({ params }) {
   const { slug } = params;
